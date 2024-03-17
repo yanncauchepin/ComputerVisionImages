f819c3c2c4e31f9b2a69be4b4da53f502135000c
yanncauchepin <cauchepin.yann@gmail.com>
2024-03-17 21:45:01 +0100

    init

---
+++ samples/classifier_covid_lungs/preprocessing.py
@@ -0,0 +1,28 @@
+import os
+import cv2
+
+root_path = "/media/yanncauchepin/ExternalDisk/Datasets/ComputerVisionImages/classifier_covid_lungs/"
+
+def load_dataframe() :
+    
+    images = []
+    labels = []
+
+    for set_ in ['train', 'test']:
+        for label in os.listdir(os.path.join(root_path, set_)):
+            label_dir = os.path.join(root_path, set_, label)
+            for image_file in os.listdir(label_dir):
+                image_path = os.path.join(label_dir, image_file)
+                image = cv2.imread(image_path)
+                if image is not None:
+                    images.append(image)
+                    labels.append(str(label).lower())
+
+    return {"images" : images, "labels" : labels}
+
+
+if __name__ == '__main__' :
+
+    """EXAMPLE"""
+
+    df_covid_lungs = load_dataframe()
